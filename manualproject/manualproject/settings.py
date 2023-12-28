bb2d52d466d27e5bca82669337127ea125786676
nyakako <nyakako13@gmail.com>
2023-12-28 14:45:13 +0900

    ローカル変更保存

--- manualproject/manualproject/settings.py
+++ manualproject/manualproject/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e%f09v$zp49rq73!xf=sw!#=x89x@$mh%1_koh&z2#z2^wx%oj"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
@@ -77,20 +83,7 @@ WSGI_APPLICATION = "manualproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-    # "default": {
-    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
-    #     "NAME": "manual_viewer",
-    #     "USER": "postgres",
-    #     "PASSWORD": "********",
-    #     "HOST": "localhost",
-    #     "PORT": "5432",
-    # }
-}
+DATABASES = {"default": env.db()}
 
 
 # Password validation
